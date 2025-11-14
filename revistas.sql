43e491a6ec49bca8d3975d7438e02fa74fab729e
ruiricca <ruiricca@gmail.com>
2025-11-14 15:52:59 +0000

    Create revistas.sql

---
+++ revistas.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE revistas;
+USE revistas;
+
+CREATE TABLE artigo(
+	id_artigo VARCHAR(3) PRIMARY KEY,
+  titulo VARCHAR(20) NOT NULL,
+  id_tipo_artigo VARCHAR(3) NOT NULL,
+  pag INT NOT NULL,
+  FOREIGN KEY (id_tipo_artigo) REFERENCES tipo_artigo (id_tipo_artigo)
+); 
