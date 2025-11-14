39b4df4d6adb70a8b88fc0a9b85be42776de4c65
ruiricca <ruiricca@gmail.com>
2025-11-14 16:25:52 +0000

    Update revistas.sql

--- revistas.sql
+++ revistas.sql
@@ -3,7 +3,7 @@ CREATE DATABASE revistas;
 USE revistas;
 
 CREATE TABLE artigo(
-	id_artigo VARCHAR(3) PRIMARY KEY,
+  id_artigo VARCHAR(3) PRIMARY KEY,
   titulo VARCHAR(20) NOT NULL,
   id_tipo_artigo VARCHAR(3) NOT NULL,
   pag INT NOT NULL,
